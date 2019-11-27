97760258db4f0ec2655a7c9f719a43100367ca16
Sourabh Cheedella <cheedella.sourabh@gmail.com>
2019-11-27 15:28:22 -0500

    use string formatting for decimal precision

--- shakemap/mapping/mapmaker.py
+++ shakemap/mapping/mapmaker.py
@@ -110,63 +110,10 @@ IMT_CMAP = 'PuRd'
 
 def to_precision(x, p):
     """
-    returns a string representation of x formatted with a precision of p
-
-    Based on the webkit javascript implementation taken from here:
-    https://code.google.com/p/webkit-mirror/source/browse/JavaScriptCore/kjs/number_object.cpp
+    returns a string representation of x formatted with a precision of p.
     """
 
-    x = float(x)
-
-    if x == 0.:
-        return "0." + "0"*(p-1)
-
-    out = []
-
-    if x < 0:
-        out.append("-")
-        x = -x
-
-    e = int(np.log10(x))
-    tens = np.power(10, e - p + 1)
-    n = np.floor(x/tens)
-
-    if n < np.power(10, p - 1):
-        e = e - 1
-        tens = np.power(10, e - p+1)
-        n = np.floor(x / tens)
-
-    if abs((n + 1.) * tens - x) <= abs(n * tens - x):
-        n = n + 1
-
-    if n >= np.power(10, p):
-        n = n / 10.
-        e = e + 1
-
-    m = "%.*g" % (p, n)
-
-    if e < -2 or e >= p:
-        out.append(m[0])
-        if p > 1:
-            out.append(".")
-            out.extend(m[1:p])
-        out.append('e')
-        if e > 0:
-            out.append("+")
-        out.append(str(e))
-    elif e == (p - 1):
-        out.append(m)
-    elif e >= 0:
-        out.append(m[:e+1])
-        if e+1 < len(m):
-            out.append(".")
-            out.extend(m[e+1:])
-    else:
-        out.append("0.")
-        out.extend(["0"]*-(e+1))
-        out.append(m)
-
-    return "".join(out)
+    return "{0:." + str(p) + "f}".format(x)
 
 
 def _create_palette(imtype, levels):
